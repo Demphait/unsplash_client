7f52d750943bc0e99f1bdeca6bec6eef4b542d1e
Bohdan Plastun <bodya13371337@gmail.com>
2023-04-08 11:30:22 +0300

    feat: added displaying of photos from HTTP request

--- lib/views/feed/feed_view.dart
+++ lib/views/feed/feed_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:unsplash_client/api/http_service.dart';
+import 'package:unsplash_client/models/photos_model.dart';
 import 'package:unsplash_client/views/feed/widgets/photo_item.dart';
+import 'package:unsplash_client/widgets/app_loader.dart';
 
 class FeedView extends StatefulWidget {
   const FeedView({super.key});
@@ -9,18 +12,45 @@ class FeedView extends StatefulWidget {
 }
 
 class _FeedViewState extends State<FeedView> {
-  String imageUrl =
-      'https://images.unsplash.com/photo-1680849219172-71e4f632b282?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3174&q=80';
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Feed'),
+        centerTitle: true,
       ),
-      body: ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          return PhotoItem(photo: imageUrl);
+      body: FutureBuilder(
+        future: HttpService.instance.fetchPhotos(),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<PhotoModel>?> snapshot) {
+          if (snapshot.hasData && snapshot.data != null) {
+            List<PhotoModel> photoModels = snapshot.data!;
+            return ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: photoModels.length,
+              itemBuilder: (BuildContext context, int index) {
+                return PhotoItem(photo: photoModels[index]);
+              },
+            );
+          } else {
+            return HttpService.instance.error.isEmpty
+                ? Center(child: AppLoader())
+                : Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(HttpService.instance.error),
+                        TextButton(
+                          onPressed: () {
+                            HttpService.instance.fetchPhotos();
+                            setState(() {});
+                          },
+                          child: const Text('Try again'),
+                        ),
+                      ],
+                    ),
+                  );
+          }
         },
       ),
     );
